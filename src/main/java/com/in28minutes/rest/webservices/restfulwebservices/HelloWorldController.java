cbf1c93b6020b2e6a46cf990b1624424f910fe1a
Kabir Bharti <kbharti@redhat.com>
2021-10-13 14:03:42 -0500

    Update HelloWorldController.java

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,5 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
-
+import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +8,14 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/")
 	public String helloWorld() {
+		
+		try{
+			ArrayList<String> list = new ArrayList<String>();
+			list.get(1);
+			}
+		catch(Exception e){
+			e.printStackTrace();	
+		}
 		return "{\"message\":\"Hello World Java v1\"}";
 	}
 }
